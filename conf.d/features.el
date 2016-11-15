d27127daed42c250302a8378b083c8ca4574853a
Afay Zangetsu <afay.zangetsu@gmail.com>
2016-11-15 20:00:22 +0300

    lastest changes ...

--- conf.d/features.el
+++ conf.d/features.el
@@ -14,6 +14,7 @@
 (unless (version< emacs-version "24.4")
   (use-package magit :ensure t :defer t
     :config (setq magit-completing-read-function 'magit-ido-completing-read))
+  (use-package git-timemachine :ensure t :defer t)
   (use-package docker :defer t :ensure t :config (docker-global-mode)))
 
 ;; ============================== Jump =========================================
